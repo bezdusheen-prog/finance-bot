ad9ac71ed616359ec7bc9e2d446cf0f0ec7982c9
bezdusheen-prog <bezdusheen@gmail.com>
2026-06-08 09:10:31 +0300

    Revert to working version - fix syntax error at line 190

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from dotenv import load_dotenv
- 
+
 # Загрузка переменных окружения
 load_dotenv()
 
